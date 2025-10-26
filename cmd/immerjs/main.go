16f51868492276c553e68c607cd54be1a1f00f68
Dema <demaagro@hotmail.com>
2025-10-27 03:40:36 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/immerjs/main.go
@@ -0,0 +1,23 @@
+// cmd/immerjs/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "immerjs/internal/immerjs"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := immerjs.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
